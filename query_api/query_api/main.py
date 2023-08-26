918c9713fe7b4ed86e5ec171c9701b5e90dc3540
AW <agataweglik@gmail.com>
2023-08-26 16:17:48 +0200

    new branch for containers connection

--- query_api/query_api/main.py
+++ query_api/query_api/main.py
@@ -7,7 +7,15 @@ from confluent_kafka.cimpl import NewTopic, Producer
 
 #remove query_api to run with uvicorn - if not: query_api.utils.sample_weather
 from query_api.utils.sample_weather import sample_weather
-from query_api.utils.actual_weather import actual_weather
+# from query_api.utils.actual_weather import actual_weather
+
+#new branch
+import asyncio
+import time
+import requests
+from fastapi.responses import StreamingResponse
+
+api_url = "https://api.openweathermap.org/data/2.5/weather?"
 
 load_dotenv()
 
@@ -15,13 +23,27 @@ p = Producer({'bootstrap.servers': os.getenv('KAFKA_BROKER')})
 
 app = FastAPI()
 
-# @app.on_event('startup')
-# async def api_query_startup():
-#     a = AdminClient({'bootstrap.servers': os.getenv('KAFKA_BROKER')})
-#     topic_list=[]
-#     topic_list.append(NewTopic(topic="weather_data", num_partitions=1, replication_factor=1))
-#     a.create_topics(new_topics=topic_list, validate_only=False)
+isOpen = True
 
+async  def actual_weather_async(locations: list):
+    i=0
+    global isOpen
+    while (i < 2):
+        i+=1
+        for location in locations:
+            await asyncio.sleep(1)
+            if (isOpen == False):
+                isOpen = True
+                return
+            response = requests.get(api_url + "lat=" + str(location[0]) + "&lon=" + str(location[1]) +"&appid=" + os.getenv('WEATHER_API_KEY'))
+            response_json = response.json()
+            latitude = location[0]
+            longtitude = location[1]
+            temperature = response_json["main"]["temp"] - 273.15
+            data = json.dumps({"lat":latitude, "lon":longtitude, "temp":temperature})
+            p.produce('weather_data', data)
+            p.flush()
+            yield data
 
 @app.get("/")
 async def root():
@@ -30,12 +52,10 @@ async def root():
 @app.get("/weather/{country}")
 async def weather(country: str):
     locations, number = sample_weather(country)
-    response = actual_weather(locations)
-    content = [value.content for value in response]
-    return {"message":  content, "number of geohashes": number}
+    return StreamingResponse(actual_weather_async(locations))
 
-@app.get("/test/{data}")
-async def testkafka(data):
-    p.produce('weather_data', json.dumps({"demo": f"{data}"}))
-    p.flush()
+@app.get("/stop")
+async def stopdata():
+    global isOpen
+    isOpen = False
 
