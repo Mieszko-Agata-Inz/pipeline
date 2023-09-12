88167a25ad638b2d99a5b5915b6c186978a9c101
Mieszko <mirgosmieszko@gmail.com>
2023-09-12 19:54:57 +0200

    objects transferred to redis as jsons

--- redis_caching/redis_caching/redis_caching.py
+++ redis_caching/redis_caching/redis_caching.py
@@ -1,7 +1,7 @@
 import redis
 # import os
 import time
-
+import json
 from kafka import KafkaConsumer
 
 time.sleep(40)
@@ -44,13 +44,43 @@ redisCli = redis.Redis(
     password = "eYVX7EwVmmxKPCDmwMtyKVge8oLd2t81"
     )
 # kafka consumer 
+n=0
 kafka_consumer = KafkaConsumer('weather_data_output', bootstrap_servers='kafka:29092')
+kafka_consumer_2 = KafkaConsumer('raw_weather_data', bootstrap_servers='kafka:29092')
 
-for message in kafka_consumer:
-    data = message.value
-    key = message.key
-    print(data)
-    value = redisCli.set(key, data)
+# for message in kafka_consumer: redis-cli -a 'eYVX7EwVmmxKPCDmwMtyKVge8oLd2t81'
+#     data = message.value
+#     key = message.key
+#     print(data)
+#     value = redisCli.set(key, data)
+
+def process_summary(message):
+    global n
+    print(message)
+    for key, value in message.items():
+        for consumer_record in value:
+            redisCli.json().set(f"s{n}", '$',json.dumps(consumer_record.value.decode('utf-8')))
+            n+=1
+
+
+def process_raw(message):
+    global n
+    print(message)
+    for key, value in message.items():
+        for consumer_record in value:
+            redisCli.json().set(f"r{n}", '$', json.dumps(consumer_record.value.decode('utf-8')))
+            n+=1
+
+timeout = 100
+while True:
+    message1 = kafka_consumer.poll(timeout_ms=timeout)
+    if message1:
+        process_summary(message1)
+
+
+    message2 = kafka_consumer_2.poll(timeout_ms=timeout)
+    if message2:
+        process_raw(message2)
 
 
 # writing
@@ -59,3 +89,6 @@ for message in kafka_consumer:
 
 # valueout = redisCli.get("key0")
 # print(valueout)
+
+
+
