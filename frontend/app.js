aab265051eaa9e38893e52c067229ddb45e01489
AW <agataweglik@gmail.com>
2023-12-27 17:44:39 +0100

    frontend delay

--- frontend/app.js
+++ frontend/app.js
@@ -1,15 +1,11 @@
 var express = require("express");
 var path = require("path");
-var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-const https = require('http');
 
 var app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public-flutter")));
 // // Add headers before the routes are defined
 app.use(function (req, res, next) {
@@ -21,9 +17,11 @@ app.use(function (req, res, next) {
     next();
 });
 
-const port = 8080; // Choose a suitable port number
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
 
-module.exports = app;
+setTimeout(function () {
+    const port = 8080;
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
+}, 70000); // 70 seconds of sleep just to wait for data in pipeline for first predictions
+
